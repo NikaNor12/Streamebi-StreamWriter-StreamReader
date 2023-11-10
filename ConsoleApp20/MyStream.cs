3942b765af5611ce324a3d016e63a4fca0005556
Nika Chichua <chichonika9@gmail.com>
2023-11-10 06:22:32 -0800

    Add project files.

---
+++ ConsoleApp20/MyStream.cs
@@ -0,0 +1,38 @@
+﻿namespace ConsoleApp20
+{
+    class MyStream
+    {
+        public void Process()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            StreamWriter writer = new StreamWriter(@"D:\G19.txt");
+            writer.WriteLine("Nika");
+            writer.WriteLine("Elene");
+            writer.WriteLine("Gio");
+            writer.WriteLine("Gela");
+            writer.WriteLine("Dgnachvi");
+            writer.Close();
+
+            StreamReader reader = new StreamReader(@"D:\G19.txt");
+            StreamWriter writer2 = new StreamWriter(@"D:\G19_2.txt");
+
+            int i = 1;
+            string? line = reader.ReadLine();
+
+            while (line != null)
+            {
+                line = $"{i++}.{line}";
+                writer2.WriteLine(line);
+                Console.WriteLine(line);
+                line = reader.ReadLine();
+            }
+            
+            reader.Close();
+            writer2.Close();
+            Console.ResetColor();
+        }
+    }
+}
+
+
---
+++ ConsoleApp20/Program.cs
@@ -0,0 +1,13 @@
+﻿using ConsoleApp20;
+
+namespace G19_20231101
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyStream stream = new MyStream();
+            stream.Process();
+        }
+    }
+}
